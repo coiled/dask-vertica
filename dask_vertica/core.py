d7ed45f9b33f80f5cd13cf098a78cd090f7321cf
Paul Hobson <pmhobson@gmail.com>
2022-03-03 22:13:39 -0800

    remove ensure_db_exists [ci skip]

--- dask_vertica/core.py
+++ dask_vertica/core.py
@@ -18,7 +18,6 @@ from verticapy import vDataFrame
 from verticapy.utilities import pandas_to_vertica, drop, readSQL
 import vertica_python
 from vertica_python.vertica.connection import Connection
-from vertica_python.errors import DuplicateObject
 
 
 @delayed
@@ -52,7 +51,6 @@ def daskdf_to_vertica(
                 parse_n_lines=parse_n_lines
             )
 
-
             vdf.to_db(
                 f'"{schema}"."{name}"',
                 df.columns.tolist(),
@@ -60,35 +58,6 @@ def daskdf_to_vertica(
                 inplace=False,
             )
             _drop_table(connection, tmp, schema=schema)
-
-
-@delayed
-def ensure_db_exists(
-    df: pd.DataFrame,
-    connection: Connection,
-    name: str,
-    schema: str = "public",
-    parse_n_lines: int = 10_000,
-    inplace: bool = True,
-):
-    # NOTE: we have a separate `ensure_db_exists` function in order to use
-    # pandas' `to_sql` which will create a table if the requested one doesn't
-    # already exist.
-    with connection.cursor() as cursor:
-            vdf = pandas_to_vertica(
-                df,
-                cursor=cursor,
-                name=name,
-                schema=schema,
-                parse_n_lines=parse_n_lines
-            )
-            vdf.to_db(
-                f'"{schema}"."{name}"',
-                df.columns.tolist(),
-                relation_type="table",
-                inplace=inplace,
-            )
-            return True
 
 
 def _check_if_exists(
@@ -164,7 +133,6 @@ def to_vertica(
     table_already_exists = _check_if_exists(connection, name, schema=schema)
 
     if not table_already_exists:
-        # ensure_db_exists(df.head(), connection, name, schema=schema).compute()
         pass
 
     else:
@@ -172,7 +140,6 @@ def to_vertica(
             raise RuntimeError(f"Table {name} already exists in {schema}")
         elif if_exists == "overwrite":
             _drop_table(connection, name, schema=schema)
-            # ensure_db_exists(df.head(), connection, name, schema=schema).compute()
 
     dask.compute(
         [
